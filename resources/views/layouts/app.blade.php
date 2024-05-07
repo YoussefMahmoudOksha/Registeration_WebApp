415ce3ac15de00758272b6c5140e7a22b8bc5b78
YoussefMahmoudOksha <http://youssef.mahmoud75359@gmail.com/>
2024-05-07 23:56:27 +0300

    add vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'User Registration')</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <script src="{{ asset('js/API_Ops.js') }}"></script>
-    <script src="{{ asset('js/validation.js') }}"></script>
-
+    {{-- <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <script src="{{ asset('js/API_Ops.js') }}"></script>                      {{-- if you don't have nbm use this 3 lines --}}
+    {{--<script src="{{ asset('js/validation.js') }}"></script> --}}
+    @vite(['resources/css/styles.css', 'resources/js/validation.js','resources/js/API_Ops.js'])    {{-- if you hava nbm --}}
 
 </head>
 <body>
