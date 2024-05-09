dce18ddc2f8998d42770e0752af3da6e08864672
Youssef18118 <101882887+Youssef18118@users.noreply.github.com>
2024-05-09 20:42:14 +0300

    Adding Lang Localization

---
+++ lang/ar/passwords.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+
+    
+    'reset' => 'تم اعادة تعيين كلمة السر',
+    'sent' => 'لقد قمنا بارسال لينك الي بريدك الالكتروني المسجل',
+    'throttled' => 'من ضلك قم بالانتظار قبل المحاولة مرة اخري',
+    'token' => 'رمز اعادة تعيين كلمة السر غير صحيح',
+    'user' => "لا يمكننا العثور علي مستخدم لهذا البريد الالكتروني",
+
+];
---
+++ lang/en/myCustom.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to FCAI Student Registration', 
+    'Home' => 'Home',
+    'About' => 'About',
+    'Contact' => 'Contact',
+    'Registration' => 'User Registration. All rights reserved.',
+    'Form' => 'User Registration Form',
+    'FullName' => 'Full Name:',
+    'UserName' => 'User Name:',
+    'Birthday' => 'Birthday:', 
+    'Phone' => 'Phone:',
+    'Address' => 'Address:',
+    'Email' => 'Email:',
+    'Password' => 'Password:',
+    'ConfirmPassword' => 'Confirm Password:',
+    'UserImage' => 'User Image:',
+    'required' => 'All fields are required.',
+    'Register' => 'Register',
+
+];
+
+
+
+
+
+?>
